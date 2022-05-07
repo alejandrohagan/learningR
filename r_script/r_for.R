b678958f87f8aae52e35b9fe89ed25c5c11d38cd
haganistic <alejandro.hagan@outlook.com>
2022-05-07 22:39:12 +0000

    initial commit

--- r_script/r_for.R
+++ r_script/r_for.R
@@ -5,6 +5,29 @@ install.packages("pacman")
 
 pacman::p_load(tidyverse,filesstrings,scales)
 
-a <- str(mpg)
+a <- str()
+parse_number
 
-parse_number()
+mad
+
+library(listviewer)
+jsonedit(mad)
+a <- lm(formula= cty ~ hwy,data = mpg)
+b <- jsonedit(a)
+summary(b)
+summary(a)
+pull(b,1)
+
+library(tidyr)
+install.packages("repurrrsive")
+library(repurrrsive)
+got_chars %>% map("name")
+
+got_chars %>%
+  map_chr('name')
+
+set_names( )
+
+got_chars[1:5] %>%
+  set_names(map_chr(.,'name')) %>%
+  listviewer::jsonedit()
